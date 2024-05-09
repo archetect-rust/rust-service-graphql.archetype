a96a3bf9b0dce52c452d0cc1f3b4553ed1415b11
Jimmie Fulton <jimmie.fulton@gmail.com>
2024-05-09 15:54:35 -0700

    Process new .proto files in specs/clients automatically

--- contents/base/{{ project-name }}/crates/{{ project_name }}_core/build.rs
+++ contents/base/{{ project-name }}/crates/{{ project_name }}_core/build.rs
@@ -1,29 +1,40 @@
-use std::env;
+use std::{env, fs};
+use std::ffi::OsStr;
+use std::fs::DirEntry;
 use std::path::PathBuf;
 
-{%- for application_key in applications %}
-{%- set application = applications[application_key] %}
-const {{ application['PROJECT_NAME'] }}_PROTO: &str = "../../specs/clients/{{ application['project_name'] }}.proto";
-{%- endfor %}
 const CLIENTS_DIR: &str = "../../specs/clients";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let out_dir = PathBuf::from(env::var("OUT_DIR").expect("Valid OUT_DIR"));
+    let clients_dir = PathBuf::from(CLIENTS_DIR);
 
-    {%- for application_key in applications %}
-    {%- set application = applications[application_key] %}
-
-    println!("cargo:rerun-if-changed={}", {{ application['PROJECT_NAME'] }}_PROTO);
-
-    tonic_build::configure()
-        .file_descriptor_set_path(out_dir.join("{{ application['project_name'] }}.bin"))
-        .build_server(false)
-        .build_client(true)
-        .compile(&[{{ application['PROJECT_NAME'] }}_PROTO],
-                 &[CLIENTS_DIR]
-        )
-        .unwrap();
-    {%- endfor %}
+    for proto_path in fs::read_dir(clients_dir)?.flatten()
+        .filter(has_proto_extension)
+        .map(|entry| entry.path())
+    {
+        let proto_path_string = proto_path.to_str()
+            .expect("Valid Proto File Path");
+        let descriptor_file_name = proto_path.file_stem()
+            .map(|stem| stem.to_str())
+            .flatten()
+            .expect("Pre-validated File Stem")
+            ;
+        let descriptor_path = out_dir.join(descriptor_file_name);
+        println!("cargo:rerun-if-changed={}", proto_path_string);
+        tonic_build::configure()
+            .file_descriptor_set_path(descriptor_path)
+            .build_server(false)
+            .build_client(true)
+            .compile(&[proto_path],
+                     &[CLIENTS_DIR],
+            )
+            .unwrap();
+    }
 
     Ok(())
+}
+
+fn has_proto_extension(entry: &DirEntry) -> bool {
+    entry.path().extension().eq(&Some(OsStr::new("proto")))
 }
